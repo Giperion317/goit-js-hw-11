08a6ad229e9b638a489676c89f1ecfa0912b39cb
Anatolii Pismennyi <105397905+Giperion317@users.noreply.github.com>
2022-10-27 03:49:07 +0200

    Upload gallery rendering

--- src/index.js
+++ src/index.js
@@ -11,19 +11,76 @@ loadMoreBtn.addEventListener('click', onLoadMore);
 function onSearch(event) {
   event.preventDefault();
 
-  newsApiService.query = event.target.elements.searchQuery.value;
+  newsApiService.query = event.target.elements.searchQuery.value.trim();
 
-  newsApiService
-    .getGalery()
-    .then(pictures => console.log(pictures.data.hits))
-    .catch(error => console.log(error));
-}
+  if (newsApiService.query === '') {
+    clearGallery();
+    console.log('Fill in the search bar!');
+    return;
+  }
 
-function onLoadMore(event) {
+  newsApiService.resetPage();
+  clearGallery();
+
   newsApiService
     .getGalery()
     .then(pictures => {
-      console.log(pictures.data.hits);
+      console.log(pictures);
+      if (pictures.length === 0) {
+        console.log(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      }
+      createGallery(pictures);
     })
     .catch(error => console.log(error));
+}
+
+function onLoadMore() {
+  newsApiService.incrementPage();
+  newsApiService
+    .getGalery()
+    .then(pictures => {
+      console.log(pictures);
+      createGallery(pictures);
+    })
+    .catch(error => console.log(error));
+}
+
+function createGallery(pictures) {
+  const markup = pictures
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `<div class="photo-card">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+    <b>Likes</b><span class="info-value">${likes}</span>
+    </p>
+    <p class="info-item">
+      <b>Views</b><span class="info-value">${views}</span>
+    </p>
+    <p class="info-item">
+      <b>Comments</b><span class="info-value">${comments}</span>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b><span class="info-value">${downloads}</span>
+    </p>
+  </div>
+</div>`
+    )
+    .join();
+
+  gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+function clearGallery() {
+  gallery.innerHTML = '';
 }
--- src/news-servise.js
+++ src/news-servise.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
+const API_KEY = '30836581-5201b2d4b86300260b3de2e16';
+const BASE_URL = 'https://pixabay.com/api/';
+
 export default class NewsApiService {
   constructor() {
     this.searchQuery = '';
@@ -7,21 +10,27 @@ export default class NewsApiService {
   }
 
   async getGalery() {
-    const BASE_URL = 'https://pixabay.com/api/';
-    const option = {
+    const options = {
       params: {
-        key: '30836581-5201b2d4b86300260b3de2e16',
+        key: API_KEY,
         q: `${this.searchQuery}`,
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: 'true',
-        per_page: 100,
+        per_page: 40,
         page: this.page,
       },
     };
-    const pictures = await axios.get(`${BASE_URL}`, option);
+    const pictures = await axios.get(`${BASE_URL}?`, options);
+    return pictures.data.hits;
+  }
+
+  incrementPage() {
     this.page += 1;
-    return pictures;
+  }
+
+  resetPage() {
+    this.page = 1;
   }
 
   get query() {
